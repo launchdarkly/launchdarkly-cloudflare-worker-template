aad1fec51513cbd598a97de19587ca05144dec7d
Henry Barrow <hbarrow@launchdarkly.com>
2022-10-13 14:51:50 +0100

    Update to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   target: "webworker",
   output: { path: path.resolve(__dirname, "worker"), filename: "script.js" },
-  node: {
-    fs: "empty",
-    tls: "empty",
-    net: "empty",
+  plugins: [new NodePolyfillPlugin()],
+  resolve: {
+    fallback: {
+      fs: false,
+      net: false,
+      tls: false,
+      yaml: false,
+    },
   },
 };
