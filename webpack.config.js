c2ede626ce99af19f0fbc4ea25b37c872375246c
Henry Barrow <hbarrow@launchdarkly.com>
2022-10-13 16:19:37 +0100

    Revert "Update to webpack 5 (#5)"
    This reverts commit 30353032008d59e4448b1525e2a7db6802ac0159.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,12 @@
 const path = require("path");
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
   target: "webworker",
   output: { path: path.resolve(__dirname, "worker"), filename: "script.js" },
-  plugins: [new NodePolyfillPlugin()],
-  resolve: {
-    fallback: {
-      fs: false,
-      net: false,
-      tls: false,
-      yaml: false,
-    },
+  node: {
+    fs: "empty",
+    tls: "empty",
+    net: "empty",
   },
 };
