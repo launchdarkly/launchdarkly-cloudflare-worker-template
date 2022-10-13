b00366fe3f23043f07040c78940d3bf2756bb78c
Henry Barrow <hbarrow@launchdarkly.com>
2022-10-13 12:13:10 +0100

    Bump launchdarkly-cloudflare-edge-sdk to v1.0.0 and update to support wrangler 2

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
-import { init } from 'launchdarkly-cloudflare-edge-sdk'
+import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
+import { init } from "launchdarkly-cloudflare-edge-sdk";
 
-let ldClient
+let ldClient;
 
 const initLdClient = () => {
-  if (ldClient) {
-    return Promise.resolve()
+  if (!ldClient) {
+    // LD_KV and LD_CLIENT_SIDE_ID are runtime variables that the Workers runtime provides to your code.
+    // For more information read https://developers.cloudflare.com/workers/platform/environment-variables
+    ldClient = init(LD_KV, LD_CLIENT_SIDE_ID);
+    return ldClient.waitForInitialization();
   }
 
-  ldClient = init(MY_KV, 'YOUR_CLIENT_SIDE_ID')
-  return ldClient.waitForInitialization()
-}
+  return Promise.resolve();
+};
 
 class FlagsStateInjector {
   async element(element) {
-    const user = { key: 'anonymous', custom: { teamId: 'anonymous' } }
-    const allFlags = await ldClient.allFlagsState(user, { clientSideOnly: true })
-    element.append(`<script>window.ldFlags = ${JSON.stringify(allFlags)}</script>`, { html: true })
+    const user = { key: "anonymous", custom: { teamId: "anonymous" } };
+    const allFlags = await ldClient.allFlagsState(user, {
+      clientSideOnly: true,
+    });
+    element.append(
+      `<script>window.ldFlags = ${JSON.stringify(allFlags)}</script>`,
+      { html: true }
+    );
   }
 }
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event))
-})
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event));
+});
 
 async function handleRequest(event) {
-  const [response] = await Promise.all([getAssetFromKV(event), initLdClient()])
-  const acceptHeader = event.request.headers.get('accept')
+  const [response] = await Promise.all([getAssetFromKV(event), initLdClient()]);
+  const acceptHeader = event.request.headers.get("accept");
 
-  if(acceptHeader && acceptHeader.includes('text/html')){
-    return new HTMLRewriter().on('head', new FlagsStateInjector()).transform(response)
+  if (acceptHeader && acceptHeader.includes("text/html")) {
+    return new HTMLRewriter()
+      .on("head", new FlagsStateInjector())
+      .transform(response);
   }
 
-  return response
+  return response;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 module.exports = {
-  entry: './index.js',
-  target: 'webworker',
+  entry: "./index.js",
+  target: "webworker",
+  output: { path: path.resolve(__dirname, "worker"), filename: "script.js" },
   node: {
-    fs: 'empty',
-    tls: 'empty',
-    net: 'empty',
+    fs: "empty",
+    tls: "empty",
+    net: "empty",
   },
-}
+};
